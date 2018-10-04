7400440ab895c978b7282df014ca697a84f260cd
Brien Mizell <40799762+brienmizell@users.noreply.github.com>
2018-10-04 15:50:39 -0400

    converted .py to .js

---
+++ scripts/hello.js
@@ -0,0 +1,26 @@
+//   Yep. It's JavaScript
+// console.log('Hello JavaScript, what is up: I am fine!');
+
+// // hello.py
+// // using var
+// // camelCase
+
+// var promptText = 'What is your name?';
+// var userName = prompt(promptText);
+
+// var greeting = 'Hello ' + userName + "!";
+// console.log(greeting);
+// use let
+
+const promptText = "What is your name?";
+var userName = prompt(promptText);
+
+const greeting = `Hello, ${userName} !`;
+console.log(greeting);
+
+
+// using sexy sentax
+
+
+
+// string interpoliation
---
+++ scripts/madlib.js
@@ -0,0 +1,10 @@
+// hHard=coding instead of prompting
+// because in real life, when you wnt
+// user input, you'll create an HTML form.
+
+const name = `chris aquino`;
+const subject = `feline husbandry`;
+
+const finalText = `${name}'s favorite subject in school is ${subject}.`;
+
+console.log(finalText);
